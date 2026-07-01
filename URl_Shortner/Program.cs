885aa7ffca86faf5b2840beea3c2546c88e1e0df
sabinstha447 <sabinstha447@gmail.com>
2026-07-01 13:45:05 +1200

    ADDED JWT AUTHENTICATION

--- URl_Shortner/Program.cs
+++ URl_Shortner/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.Extensions.Options;
 using URl_Shortner.Data;
 using Microsoft.EntityFrameworkCore;
+using URl_Shortner.Services;
 
 namespace URl_Shortner
 {
@@ -15,6 +16,7 @@ namespace URl_Shortner
             builder.Services.AddControllers();
             builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            builder.Services.AddScoped<IAuthService, AuthService>();
             builder.Services.AddOpenApi();
 
             var app = builder.Build();
--- URl_Shortner/Services/AuthService.cs
+++ URl_Shortner/Services/AuthService.cs
@@ -4,15 +4,21 @@ using Microsoft.EntityFrameworkCore;
 using BCrypt.Net;
 using URl_Shortner.Models;
 using System.ComponentModel.DataAnnotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace URl_Shortner.Services
 {
     public class AuthService: IAuthService
     {
         private readonly AppDbContext _context;
-        public AuthService(AppDbContext context)
+        private readonly IConfiguration _configuration;
+        public AuthService(AppDbContext context, IConfiguration configuration)
         {
             _context = context;
+            _configuration = configuration;
             
         }
         public async Task<string> Login(LoginRequest logged)
@@ -25,10 +31,34 @@ namespace URl_Shortner.Services
             var passwordValid = BCrypt.Net.BCrypt.Verify(logged.Password, loggedUser.PasswordHash);
             if (passwordValid)
             {
-                return "Invalid email or password";
+                return "this will become the JWT soon";
             }
            
-            return "No User Found";
+            return "Invalid email or password";
+        }
+        private string GenerateJwtToken(User user)
+        {
+            var claims = new List<Claim>{
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Email, user.Email)
+       
+            };
+            //Stamping Tool
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // Building the Card
+            var token = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                claims: claims,
+                expires : DateTime.UtcNow.AddMinutes(60),
+                signingCredentials: credentials
+
+                );
+            return new JwtSecurityTokenHandler().WriteToken(token);
+
+                
+               
         }
         public async Task<string> Register(RegisterRequest registered)
         {
